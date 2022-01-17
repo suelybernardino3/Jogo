ad644c386e616c7718da06364697248328dd928f
suely <suelybernardino@hotmail.com>
2022-01-17 16:54:48 -0300

    jogo

---
+++ app/src/main/java/com/uninassau/bilu_bilu/activity/AberturaActivity2.java
@@ -0,0 +1,85 @@
+package com.uninassau.bilu_bilu.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+import com.uninassau.bilu_bilu.R;
+
+public class AberturaActivity2 extends AppCompatActivity implements Runnable {
+
+    Thread thread;
+    Handler handler;
+    int i;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_abertura2);
+
+        handler = new Handler();
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    @Override
+    public void run(){
+        i= 1;
+
+        try {
+
+            while (i < 100){
+                Thread.sleep(15);
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        i++;
+                    }
+                });
+            }
+
+        } catch (Exception e ) {
+
+        }
+        finish();
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
+        }
+    }
+
+    private void hideSystemUI() {
+        /* Ativa o modo imersivo regular.
+         Para o modo "inclinar para trás", remova SYSTEM_UI_FLAG_IMMERSIVE.
+         Ou para "aderência imersiva", substitua por SYSTEM_UI_FLAG_IMMERSIVE_STICKY*/
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        /* Defina o conteúdo para aparecer nas barras do sistema para que o
+                         o conteúdo não é redimensionado quando as barras do sistema são ocultadas e exibidas.*/
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Oculta a barra de navegação e a barra de status
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
+    /* Mostra as barras do sistema removendo todos os sinalizadores
+    exceto para aqueles que fazem o conteúdo aparecer nas barras do sistema.*/
+    private void showSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+    }
+}
